113e665241de306622847168453f3215d772eaf4
Eric Wamari <wamari@harlertechnologies.com>
2017-07-08 11:25:45 +0300

    Added Register Activity

--- app/src/main/java/com/harlertechnologies/mkopopopote/WelcomeActivity.java
+++ app/src/main/java/com/harlertechnologies/mkopopopote/WelcomeActivity.java
@@ -1,13 +1,33 @@
 package com.harlertechnologies.mkopopopote;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    Button buttonStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        //locate the button in activity_welcome.xml
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+
+        //Capture the button click
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Start next activity
+                Intent myIntent = new Intent(WelcomeActivity.this, RegisterActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
