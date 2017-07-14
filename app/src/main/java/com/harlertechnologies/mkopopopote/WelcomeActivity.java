2c65ea8ed1971c0057a5187accecdd4268362d81
Eric Wamari <wamari@harlertechnologies.com>
2017-07-14 06:38:54 +0300

    disabled Welcome activity if you are already logged in

--- app/src/main/java/com/harlertechnologies/mkopopopote/WelcomeActivity.java
+++ app/src/main/java/com/harlertechnologies/mkopopopote/WelcomeActivity.java
@@ -1,21 +1,31 @@
 package com.harlertechnologies.mkopopopote;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     Button buttonStart;
 
     @Override
+    protected void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        
+
 
         //locate the button in activity_welcome.xml
         buttonStart = (Button) findViewById(R.id.buttonStart);
@@ -32,4 +42,17 @@ public class WelcomeActivity extends AppCompatActivity {
             }
         });
     }
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseAuth.AuthStateListener mAuthListener = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            if (firebaseAuth.getCurrentUser() != null) {
+                               //start next activity
+                Intent intent = new Intent(WelcomeActivity.this, UserActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+    };
 }
